6ad15134c83f6f1d0dccd0dc2f58aa50caf84de6
Tim Cosgrove <tim@timcosgrove.net>
2017-07-09 17:10:27 -0700

    Move away from jquery; remove currently unused packages; remove dist.

--- src/index.js
+++ src/index.js
@@ -1,69 +1,61 @@
-import _ from 'lodash';
+import SVG from 'svg.js';
+import tinycolor from 'tinycolor2';
+
 import './style.css';
-import $ from 'jquery';
 var gridSVG = require("./grid.svg");
 
 
 function component() {
   var element = document.createElement('div');
   element.innerHTML = gridSVG;
-
-
   return element;
 }
 
 document.body.appendChild(component());
 
-var hsl_string = function(hue, saturation, lightness, alpha) {
+var hsl_to_hex = function(hue, saturation, lightness, alpha) {
   hue = hue || 0;
   saturation = saturation || 100;
   lightness = lightness || 50;
-  alpha = alpha || 1
-  return "hsl(" + hue + ", " + saturation + "%, " + lightness + "%)";
+  alpha = alpha || 1;
+  return '#' + tinycolor("hsl(" + hue + ", " + saturation + "%, " + lightness + "%)").toHex();
 }
 
 var Shapes = {};
 
 function Shape(shape_id) {
   this.shape_id = shape_id;
+  this.shape_path = SVG.get(this.shape_id);
 }
 
 
   Shape.prototype.colorShift = function(transition_ms) {
-    var hue = Math.floor(Math.random() * 40 + (360));
+    var hue = Math.floor((Math.random() * 40 - 20) + (90));
     var saturation = Math.floor(Math.random() * 90) + 10;
     var lightness = Math.floor(Math.random() * 90) + 10;
     var alpha = Math.random();
-    var target = $('#' + this.shape_id);
-    target.one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function() {
-      target.css('transition', 'fill ' + transition_ms + 'ms');
-      target.css('fill', 'black');
+    this.shape_path.fill(hsl_to_hex(hue, saturation, lightness, alpha)).animate(transition_ms).fill('#000000');
 
-    });
-    target.css('transition', 'fill 1ms');
-    //target.css('opacity', alpha);
-    target.css('fill', hsl_string(hue, saturation, lightness, alpha));
   };
   Shape.prototype.colorCycle = function() {
     var shape = this;
-    var transition_ms = Math.floor(Math.random() * 4000) + 4000;
-    var cycle_ms = transition_ms - (3500);
+    var transition_ms = Math.floor(Math.random() * 2000) + 2000;
+    var cycle_ms = transition_ms + Math.floor(Math.random() * 1000);
     var timeout = setTimeout(function() {
       shape.colorShift(transition_ms);
       shape.colorCycle();
     }, cycle_ms);
   };
-  $(function() {
-    $('path').each(function() {
-      var shape_id = $(this).attr('id');
-      var initial_ms = Math.floor(Math.random() * 4000) + 4000;
-      Shapes[shape_id] = new Shape(shape_id);
 
-      Shapes[shape_id].colorShift(initial_ms);
-      Shapes[shape_id].colorCycle();
-    })
+  var paths = document.getElementsByTagName('path');
+  for (var i = 0; i < paths.length; i++) {
+    var initial_ms = Math.floor(Math.random() * 3000) + 3000;
+    var shape_id = paths[i].id;
+    Shapes[shape_id] = new Shape(shape_id);
+    Shapes[shape_id].colorShift(initial_ms);
+    Shapes[shape_id].colorCycle();   
+  }
 
-  });
 
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/'
   },
   module: {
     rules: [
@@ -34,5 +35,5 @@ module.exports = {
 
     ]
   },
-  devtool: 'source-map'
+  devtool: 'cheap-eval-source-map'
 };
